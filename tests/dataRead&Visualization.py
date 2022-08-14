bacc2c894b1367722e8eedac8f52e0a56836fdab
link-fgfgui <fgfgui_link@outlook.com>
2022-08-14 17:39:35 +0800

    Create dataRead&Visualization.py

---
+++ tests/dataRead&Visualization.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from tkinter import filedialog
+f=filedialog.askopenfile(mode="rb",title="请打开记录的inner或out文件")
+arr=__import__("pickle").load(f)
+print(len(arr))
+arr2=np.arange(0,len(arr)*3,3)
+plt.plot(arr2,arr,label="delay")
+plt.legend()
+plt.xlabel("second")
+plt.ylabel("ms")
+plt.show()
